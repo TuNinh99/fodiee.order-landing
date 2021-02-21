1d40b7b9a7c39bdd518bd4a30b5ada40116bd505
Tu Ninh <nguyentuninh217@gmail.com>
2021-02-21 14:14:53 +0700

    update new version

--- Fodiee.Order/assets/js/landingPage.js
+++ Fodiee.Order/assets/js/landingPage.js
@@ -4,8 +4,9 @@ const scrollAnimate = {
             const heightScroll = pageYOffset;
             this.showButton(heightScroll);
             this.menuAnimation(heightScroll);
-            // this.fixedBody();
-        })
+        });
+        this.mobileMenu();
+        this.runPartsAnimate();
     },
     
     //click btn to Top => scroll to Top
@@ -36,44 +37,92 @@ const scrollAnimate = {
 
     changeItemScroll(heightScroll){
         const navItems = domElement.gets('.navbar__item');
-        this.offsetsTop().map((rangeTop, index) => {
-            if(heightScroll < this.offsetsTop[0]){
-                domElement.add(navItems[0], 'active');
+        const navMobileItems = domElement.gets('.mobile__navbar-item');
+        this.offsetsTop().ranges.map((rangeTop, index) => {
+            if(heightScroll > rangeTop){ 
+                domElement.removeElements(navItems, 'active');
+                domElement.addElement(navItems[index], 'active');
             }else{
-                if(heightScroll >= rangeTop){
-                    index >= 1 ? navItems[index-1].classList.remove('active') : navItems[4].classList.remove('active');
-                    navItems[index].classList.add('active');
-                }else{
-                    navItems[index].classList.remove('active');
-                };
+
             };
         })
     },
 
     offsetsTop(){
-        const section = domElement.gets('section');
+        const sections = domElement.gets('section');
         const rangeTops = [];
-        for (let index = 0; index < section.length; index++) {
-            rangeTops.push(section[index].offsetTop - 196);
+        const ranges = [];
+
+        for (let index = 0; index < sections.length; index++) {
+            rangeTops.push(sections[index].offsetTop - sections[index].offsetHeight - 250);
+            ranges.push(sections[index].offsetTop);
         };
-        return rangeTops;
+        return {
+            rangeTops: rangeTops,
+            ranges: ranges
+        };
     },
 
-    // fixedBody(){
-    //     const body = domElement.get('body');
-    //     const open = domElement.get('.mobile__iconBars');
-    //     const close = domElement.get('.top__close');
+    mobileMenu(){
+        const open = domElement.get('.mobile__iconBars');
+        const close = domElement.get('.top__close');
 
-    //     open.onclick = (e) => {
-    //         e.preventDefault();
-    //         body.style.overflow = 'hidden';
-    //     }
+        open.onclick = (e) => {
+            e.preventDefault();
+            domElement.add('body', 'fixed');
+            domElement.add('.mobile__overlay', 'show');
+            domElement.add('.mobile__nav', 'showMobileNav');
+            domElement.add('.book', 'hidden');
+            domElement.remove('.scrollUp', 'showScrollTopBtn');
+        }
 
-    //     close.onclick = (e) => {
-    //         e.preventDefault();
-    //         body.style.overflow = 'auto';
-    //     }
-    // }
+        close.onclick = (e) => {
+            e.preventDefault();
+            domElement.remove('body', 'fixed');
+            domElement.remove('.mobile__overlay', 'show');
+            domElement.remove('.mobile__nav', 'showMobileNav');
+            domElement.remove('.book', 'hidden');
+        }
+    },
+
+    runPartsAnimate(){
+        window.addEventListener("load", this.handlerPartsAnimate);
+        window.addEventListener("resize", this.handlerPartsAnimate);
+        window.addEventListener("scroll", this.handlerPartsAnimate);
+    },
+
+    handlerPartsAnimate(){
+        const parts = domElement.gets('section .part');
+        const heightScroll = pageYOffset;
+        const rangeTops = scrollAnimate.offsetsTop().rangeTops;
+    
+        for (let i = 0; i < parts.length; i++) {
+            const children = parts[i].children; //Lấy các phần tử con của từng phần
+            const partChildrens = Array.from(children); // chuyển từ html collection sang array
+            
+            if(heightScroll >= rangeTops[i]){
+                for (let i = 0; i < partChildrens.length; i++) {
+                    if(partChildrens.length > 2){
+                        partChildrens[0].classList.add('in-down1');
+                        partChildrens[1].classList.add('in-down2');
+                        partChildrens[2].classList.add('in-down3');
+                    }else{
+                        partChildrens[0].classList.add('in-left');
+                        partChildrens[1].classList.add('in-right');
+                    }
+                }
+            }
+    
+            if(heightScroll === 0 && i !== 0){
+                domElement.removeElements(partChildrens, 'in-left');
+                domElement.removeElements(partChildrens, 'in-right');
+                domElement.removeElements(partChildrens, 'in-down1');
+                domElement.removeElements(partChildrens, 'in-down2');
+                domElement.removeElements(partChildrens, 'in-down3');
+            }
+        }
+    },
+
 }
 
 const domElement = {
@@ -92,10 +141,17 @@ const domElement = {
         const element = this.get(selector);
         element.classList.add(className);
     },
+    addElement(element, className){
+        element.classList.add(className);
+    },
 
     //Remove Class
     remove(selector, className){
         const element = this.get(selector);
+        element.classList.remove(className);
+    },
+
+    removeElement(element, className){
         element.classList.remove(className);
     },
 
@@ -104,7 +160,14 @@ const domElement = {
         elements.map(element => {
             this.remove(element, className);
         })
+    },
+
+    removeElements(elements, className){
+        for (let index = 0; index < elements.length; index++) {
+            this.removeElement(elements[index], className);
+        }
     }
 }
 
 scrollAnimate.run();
+
